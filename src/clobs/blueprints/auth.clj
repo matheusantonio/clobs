9fb7c931f9c5c9baeb88108ce5bf976aa91b335d
matheusantonio <matheus.antonio.c@outlook.com>
2020-06-23 18:30:10 -0300

    Organização dos arquivos de autenticação e apis adicionadas para verificar status da sessão.

---
+++ src/clobs/blueprints/auth.clj
@@ -0,0 +1,36 @@
+(ns clobs.blueprints.auth
+    (:require [clobs.data.users :as users-data]
+              [clojure.pprint                   :refer [pprint]]
+              [ring.util.response               :refer [response]]
+              [clobs.data.users                 :refer [password-matches?]]))
+
+
+
+;; User management
+(defn retrieve-id [username]
+    (:user/id (users-data/get-by-username username)))
+
+(defn register-user [username password]
+    (if (users-data/get-by-username username)
+        (response {:error "User already exists!"})
+        (users-data/insert username password)))
+
+;; Session management
+(defn login [username password session]
+    (if (password-matches? username password)
+        (let [new-session (assoc session :user-id (retrieve-id username))]
+          (-> (response "Logged in session!")
+              (assoc :session new-session)))
+        (response {:error "Não autorizado!"})))
+
+(defn logout []
+    (-> (response "Session deleted")
+        (assoc :session nil)))
+
+(defn current-user [session]
+    (let [id (:user-id session)]
+        (if id
+            (users-data/get-user id)
+            nil)))
+
+
--- src/clobs/core.clj
+++ src/clobs/core.clj
@@ -1,63 +1,80 @@
 (ns clobs.core
   (:require [ring.adapter.jetty               :refer [run-jetty]]
             [clojure.pprint                   :refer [pprint]]
-            [compojure.core                   :refer [routes GET POST PUT DELETE]]
+            [compojure.core                   :refer [routes context GET POST PUT DELETE]]
             [compojure.route                  :refer [not-found]]
             [ring.middleware.json             :refer [wrap-json-response wrap-json-body]]
             [ring.middleware.session          :as session]
             [ring.util.response               :refer [response]]
             [clobs.blueprints.index_bookmarks :as index]
-            [clobs.blueprints.user_bookmarks  :as user]
-            [clobs.data.users                 :refer [password-matches?]]
-            [clobs.auth                       :as auth]))
+            [clobs.blueprints.user_bookmarks  :as bookmarks]
+            [clobs.blueprints.auth            :as auth]))
 
 (def my-routes
   (routes
-    ;;home page
-    (GET "/clobs/top-bookmarks"       []              (response index/top)) ;;top 10 bookmarks
-    (GET "/clobs/recent-bookmarks"    []              (response index/recent)) ;;last 10 bookmarks added
+    (context "/clobs" []
     
-    ;;user bookmarks
-    (GET "/clobs/user"                []              (response user/get-all)) ;;index page (all user bookmarks)
-    (GET "/clobs/user/:id"            [id]            (response (user/get (Integer/parseInt id)))) ;;bookmark by id
+      ;;home page
+      (GET "/top-bookmarks"       []              (response index/top)) ;;top 10 bookmarks
+      (GET "/recent-bookmarks"    []              (response index/recent)) ;;last 10 bookmarks added
+      
 
-    (POST "/clobs/user" {:keys [body]}      ;;create a new bookmark
-      (let [{:keys [url name]} body]
-        (response (user/insert url name))))
+      ;;user bookmarks
+      (context "/bookmarks" []
+        
+        (GET "/"               []              (response bookmarks/get-all)) ;;index page (all user bookmarks)
+        (GET "/:id"            [id]            (response (bookmarks/get (Integer/parseInt id)))) ;;bookmark by id
+
+        (POST "/" {:keys [body]}      ;;create a new bookmark
+          (let [{:keys [url name]} body]
+            (response (bookmarks/insert url name))))
+        
+        (PUT "/" {:keys [body]}       ;;update an existing bookmark
+          (let [{:keys [url name id]} body]
+            (response (bookmarks/update url name id))))
+          
+        (DELETE "/:id" [id]          ;;delete an existing bookmark
+          (response (bookmarks/delete id))) 
+      )
+
+      (context "/user" []
+
+        (POST "/register" {{:keys [username password]} :body} (auth/register-user username password))
+
+
+      
+      )
+
+      ;;Authentication
+      (context "/auth" []
+      
+        (POST "/login" request
+          (let [session (:session request)
+                username (get-in request [:body :username])
+                password (get-in request [:body :password])]
+            (auth/login username password session)))
+        
+        (GET "/logout" [] (auth/logout))
+
+        (GET "/logged" {:keys [session]}
+            (response
+              (if session
+                (let [username (:user/username (auth/current-user session))]
+                  {:status 200 :message username})
+                {:status 401 :message "Não está autorizado!"})))
+      )
+
+      ;;from tutorial, for debugging
+      (POST "/debug"                    request         (response (with-out-str (clojure.pprint/pprint request))))
     
-    (PUT "/clobs/user" {:keys [body]}       ;;update an existing bookmark
-      (let [{:keys [url name id]} body]
-        (response (user/update url name id))))
-       
-    (DELETE "/clobs/user/:id" [id]          ;;delete an existing bookmark
-      (response (user/delete id))) 
+    )
 
-    (POST "/login" request
-      (let [session (:session request)
-            username (get-in request [:body :username])
-            password (get-in request [:body :password])]
-        (if (password-matches? username password)
-          (let [new-session (assoc session :username username)]
-            (-> (response "Logged in session!")
-                (assoc :session new-session)))
-          (response {:error "Não autorizado!"}))))
-
-    (GET "/logged" {:keys [session]}
-        (response
-          (if session
-            (let [username (:username session)]
-              {:status 200 :message username})
-            {:status 401 :message "Não está autorizado!"})))
-
-    ;;from tutorial
-    (POST "/debug"                    request         (response (with-out-str (clojure.pprint/pprint request))))
-    
     (not-found {:error "Not found"})))
 
 (def app
   (-> my-routes
       session/wrap-session
-      (wrap-json-body {:keywords? true})
+      (wrap-json-body {:keywords? true}) ; maps json body to clojure map
       wrap-json-response ))
 
 (defn -main
